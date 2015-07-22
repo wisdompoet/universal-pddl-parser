b5855bb57295dd144bc8a3538afe047a4572f505
wisdompoet <wisdompoet@hotmail.com>
2015-07-22 11:19:48 +0200

    temporal

--- Instance.h
+++ Instance.h
@@ -8,7 +8,7 @@ class Instance {
 public:
 	Domain &d;
 	std::string name;
-	GroundVec init, goal; // initial and goal states
+	TypeGroundVec init, goal; // initial and goal states
 	TaskVec tgoal; //goal for shop instances
 	
 	bool metric, shop;
